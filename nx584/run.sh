921606953100a868fcf488b61564f3f0273df257
doucga <garry.doucette@outlook.com>
2025-12-10 21:43:27 -0800

    Updated log

--- nx584/run.sh
+++ nx584/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/with-contenv bashio
-/usr/bin/nx584_server --config /home/nx584/config.ini --serial /dev/ttyUSB_CADDX --baudrate 38400 --listen 192.168.1.10 --port 5007 --log /home/nx584/nx585.log
+/usr/bin/nx584_server --config /usr/app/src/config.ini --serial /dev/ttyUSB_CADDX --baudrate 38400 --listen 192.168.1.10 --port 5007

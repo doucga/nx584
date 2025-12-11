754669ef88cba33825eb0cee105ebb149022a3b0
doucga <garry.doucette@outlook.com>
2025-12-10 21:29:59 -0800

    Added config
    Set configs

--- nx584/run.sh
+++ nx584/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/with-contenv bashio
-python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 10.0.0.181:10000 --config /usr/app/src/config.ini
+/usr/local/bin/nx584_server --config /home/nx584/config.ini --serial /dev/ttyUSB_CADDX --baudrate 38400 --listen 192.168.1.10 --port 5007 --log /home/nx584/nx585.log
